cde8dda340c20ac85e79602ccdc289ad02fd2e28
s188900 <s188900@stud.hioa.no>
2014-02-10 13:54:12 +0100

    lagt inn kommentarer og ryddet opp i kode.

--- oblig1/wordcloud_stl.cpp
+++ oblig1/wordcloud_stl.cpp
@@ -9,6 +9,9 @@
 
 using namespace std;
 
+/*
+ * Legger inn ulovlige chars i set stopChars.
+ */
 void init_stopChars(set<char>& list) {
   int i = 0;
   char c;
@@ -16,16 +19,19 @@ void init_stopChars(set<char>& list) {
     list.insert(c);
 }
 
+/*
+ * Legger inn ulovlige ord i set stopWords
+ */
 void init_stopWords(set<string>& list) {
   int i = 0;
   char c = *(stopwords_c+i++);
   while(c != '\0') {
     stringstream ss;
     while(c != ',') {
-      ss << c;
+      ss << c; // bygger opp ord
       c = (char) *(stopwords_c+i++);
     }
-    list.insert(ss.str());
+    list.insert(ss.str()); // setter inn ord når delimiter "," er funnet.
     c = *(stopwords_c+i++);
   }   
 }
@@ -41,23 +47,26 @@ string trimWord(string word, set<char>& invalid) {
   char c = *ptr;
   set<char>::iterator it;
   while(c != '\0') {
-    bool validchar = true;
-    for(it = invalid.begin();it != invalid.end();++it) {
+    bool validchar = true; // lar oppgit tegn skrives til stringen.
+    for(it = invalid.begin();it != invalid.end();++it) { // iterer over ulovlige bokstaver
       if(c == *it) {
-	validchar = false;
-	break;
+	validchar = false; // ignorer bokstaven
+	break; // går ur løkke
       }
     }
-    if(validchar) {
+    if(validchar) { // hvis bokstaven er lovlig, legg den til i ordet.
       c = tolower(c); // setter lower case.
       ss << c;
     }
     c = *(ptr+(++i));
   }
   string output = ss.str();
-  return output;
+  return output; // hade gått med return ss.str(), men lar den være åpen hvis operasjon skal utføres som f.eks. output.size()
 }
 
+/*
+ * Retuner utfallet om gitt ord finnes i listen av stopp-ord.
+ */
 bool is_stopWord(string word, set<string>& invalid) {
   for(set<string>::iterator it = invalid.begin(); it != invalid.end(); ++it) {
     string cmp = *it;
@@ -67,18 +76,21 @@ bool is_stopWord(string word, set<string>& invalid) {
   return false;
 }
 
+/*
+ * Sorterer ord etter count (descending) i multimap, og skriver ut de n første orden med dess count
+ */
 void print_nHighestCount(map<string,int>& words, int n) {
-  multimap<int, string, greater<int> > sortByCount;
+  multimap<int, string, greater<int> > sortByCount; // oppretter multimap som bruker compartorn std::greater
   map<string, int>::const_iterator wIter;
   char delimiter = ':';
 
   for (wIter = words.begin(); wIter != words.end(); wIter++)
-    sortByCount.insert(pair<int, string>(wIter->second, wIter->first));
+    sortByCount.insert(pair<int, string>(wIter->second, wIter->first)); // snur verdiene og setter inn count som index, med størst først
 
   multimap<int, string>::const_iterator cIter;
   for (cIter = sortByCount.begin(); cIter != sortByCount.end() && n > 0; cIter++) {
-    cout << cIter->second << delimiter << cIter->first << endl;
-    n--;
+    cout << cIter->second << delimiter << cIter->first << endl; // skriver ut ordet etterfølgt av : og sist count
+    n--; // teller ned antall element som er igjen for utskrift.
   }
 } 
 
@@ -87,34 +99,26 @@ int main(int argc, char* argv[]) {
     cout << "Missing argument; wordcloud <n top results>" << endl;
     exit(0);
   }
-  int n = atoi(argv[1]); // antall ord som skal vises.
-
+  
+  // oppretter liste for ulovlige tegn
   set<char> stopChars;
   init_stopChars(stopChars);
   
-  // test for å se om lista er riktig
-  /*set<char>::iterator it;
-  for(it = stopChars.begin(); it != stopChars.end(); ++it)
-    if('\'' == *it) cout << *it << " OK!";
-  */
+  // oppretter liste for ulovlige ord
   set<string> stopWords;
   init_stopWords(stopWords);
-  /*
-  // test
-  set<string>::iterator sit;
-  for(sit = stopWords.begin(); sit!= stopWords.end(); ++sit)
-    cout << *sit << " ";
-  */
   
   string word;
   map<string, int> words;
-  // hente ut ord
-  while(cin >> word) {
-    word = trimWord(word, stopChars);
-    if(!is_stopWord(word, stopWords)) {
-      words[word]++;
+  
+  while(cin >> word) { // leser av ord for ord 
+    word = trimWord(word, stopChars); // trimmer ordet på ulovlige tegn og setter alle bokstaver til lower case.
+    if(!is_stopWord(word, stopWords)) { // hvis ordet ikke er ulovlig
+      words[word]++; // inkrementerer counten på oppgitt ord i mapen.
     }
   }
 
-  print_nHighestCount(words, n);
+  int n = atoi(argv[1]); // antall ord som skal vises.
+  print_nHighestCount(words, n); // skriver ut n antall ord med størst count.
+  exit(0);
 }
